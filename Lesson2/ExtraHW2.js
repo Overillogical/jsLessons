5384d0dcf7a076f8e2feb3c20f077af803ab5049
AlexanderS <ksellos99@gmail.com>
2026-07-09 03:53:39 +0300

    Fixed logic of indentifying leap year

--- Lesson2/ExtraHW2.js
+++ Lesson2/ExtraHW2.js
@@ -2,12 +2,9 @@
 // високосный ли год (делится на 4, но если делится на 100 — то не високосный, кроме случаев деления на 400),
 // и вывести true/false в переменную isLeapYear.
 
-let year = 1600;
+let year = 2024;
 let isLeapYear=false;
-if(year%4===0&&year<=99){
-    isLeapYear=true;
+if(year%4===0&&year%100!=0||year%400===0){
+    isLeapYear=true;}
 
-}else if(year%100===0&&year%400===0){
-    isLeapYear=true;
-}
-console.log(isLeapYear)
+console.log(isLeapYear);
